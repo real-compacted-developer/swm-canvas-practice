00a8656bec4f8e9437ded285534c982a065f2766
SkyLightQP <combbm@naver.com>
2020-07-14 21:45:16 +0900

    feat: 팬 색깔 선택을 위한 ColorPicker 컴포넌트 추가
    feat: 기본 스타일 적용을 위해 global.css 추가

---
+++ frontend/src/components/ColorPicker.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ConfirmButton = styled.button`
+  border: none;
+  width: 80px;
+  height: 30px;
+  background-color: #4834d4;
+  color: white;
+  cursor: pointer;
+  font-size: 12px;
+
+  &:hover {
+    background-color: #686de0;
+  }
+`;
+
+const TextStyle = styled.p`
+  line-height: 30px;
+  margin-right: 1rem;
+`;
+
+const ColorPicker = () => {
+  return (
+    <Container>
+      <TextStyle>팬 색깔 (HEX)</TextStyle>
+      <input type="text" placeholder="#FFFFFF" id="pickcolor" />
+      <div id="color-holder" class="color-holder call-picker"></div>
+      <ConfirmButton>변경</ConfirmButton>
+    </Container>
+  );
+};
+
+export default ColorPicker;
